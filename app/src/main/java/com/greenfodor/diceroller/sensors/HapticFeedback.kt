2066a7c077c777cde667fc3c8a536282fbd936f9
George Fodor <georgefodor2013@gmail.com>
2026-04-24 19:43:52 +0300

    do not play haptic feedback if the device is on silent mode

--- app/src/main/java/com/greenfodor/diceroller/sensors/HapticFeedback.kt
+++ app/src/main/java/com/greenfodor/diceroller/sensors/HapticFeedback.kt
@@ -1,6 +1,7 @@
 package com.greenfodor.diceroller.sensors
 
 import android.content.Context
+import android.media.AudioManager
 import android.os.Build
 import android.os.VibrationEffect
 import android.os.Vibrator
@@ -10,6 +11,9 @@ private val waveformTimings = longArrayOf(0, 20, 104, 20, 114, 25, 122, 25, 141,
 private val waveformAmplitudes = intArrayOf(0, 60, 0, 80, 0, 100, 0, 120, 0, 140, 0, 160, 0, 180, 0, 255)
 
 fun Context.performRollHaptics() {
+    val audioManager = getSystemService(Context.AUDIO_SERVICE) as AudioManager
+    if (audioManager.ringerMode == AudioManager.RINGER_MODE_SILENT) return
+
     val vibrator = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
         val vibratorManager = getSystemService(Context.VIBRATOR_MANAGER_SERVICE) as VibratorManager
         vibratorManager.defaultVibrator
