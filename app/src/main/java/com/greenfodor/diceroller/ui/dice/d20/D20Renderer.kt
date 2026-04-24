c4414a7b342b3dcc7dcd66a07edaa8874c5b5f00
George Fodor <georgefodor2013@gmail.com>
2026-04-24 23:22:58 +0300

    refactor code based on detekt

--- app/src/main/java/com/greenfodor/diceroller/ui/dice/d20/D20Renderer.kt
+++ app/src/main/java/com/greenfodor/diceroller/ui/dice/d20/D20Renderer.kt
@@ -266,8 +266,8 @@ private fun drawFaceLabel(
 
         // Draw underline for 6 and 9 to distinguish them
         if (label == "6" || label == "9") {
-            val textY =
-                -(paints.textPaint.descent() + paints.textPaint.ascent()) / 2 + DiceConstants.D20_TEXT_BASELINE_ADJUSTMENT
+            val textY = -(paints.textPaint.descent() + paints.textPaint.ascent()) / 2 +
+                DiceConstants.D20_TEXT_BASELINE_ADJUSTMENT
             val underlineY = textY + paints.textPaint.descent() + DiceConstants.D20_UNDERLINE_TOP_OFFSET_UV
 
             drawRect(
